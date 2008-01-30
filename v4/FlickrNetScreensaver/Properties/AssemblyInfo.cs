7b973f357ec9ceb30707cd4393c0ffda6371ecbc
Sam Judson <samjudson@gmail.com>
2008-01-30 09:34:44 +0000

    git-svn-id: https://flickrnetscreensaver.svn.codeplex.com/svn@14359 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ v4/FlickrNetScreensaver/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Flickr.Net Screensaver")]
+[assembly: AssemblyDescription("A windows screensaver for displaying photos from Flickr.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sam Judson (sam@wackylabs.net)")]
+[assembly: AssemblyProduct("Flickr.Net Screensaver")]
+[assembly: AssemblyCopyright("Copyright © 2008 Sam Judson")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f75c203e-42fc-4f68-af80-3e7bbd0f88e2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("4.2.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
